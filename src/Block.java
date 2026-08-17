54d862f8c662c8609a632836342361e5782f06d3
prajit003 <prajitr003@gmail.com>
2026-08-17 13:41:11 +0530

    Add Block class

---
+++ src/Block.java
@@ -0,0 +1,46 @@
+import java.security.MessageDigest;
+
+public class Block {
+
+    String data;
+    String previousHash;
+    String hash;
+    long timestamp;
+    int nonce;
+
+    public Block(String data, String previousHash) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timestamp = System.currentTimeMillis();
+        this.nonce = 0;
+
+        this.hash = calculateHash();
+    }
+
+    public String calculateHash() {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+            String input = data + previousHash + timestamp + nonce;
+
+            byte[] hashBytes = digest.digest(input.getBytes());
+
+            StringBuilder hexString = new StringBuilder();
+
+            for (byte b : hashBytes) {
+                String hex = Integer.toHexString(0xff & b);
+
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+
+                hexString.append(hex);
+            }
+
+            return hexString.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
